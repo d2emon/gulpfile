e07d733d45679515c9905e406ae9cc3e3874ef30
Dmitry Kutsenko <d2emonium@gmail.com>
2017-10-01 10:20:08 +0300

    Images subfolders

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,10 +1,10 @@
-gulp.task('img', function(){
-  return gulp.src('assets/img/*')
-    .pipe(gulp.dest('static/img'));
-});
-
 //Prepare images
-gulp.task('images', ['img'], function(){
-return gulp.src('assets/images/*')
- .pipe(gulp.dest('static/images'));
+gulp.task('images', function(){
+return gulp.src([
+    'assets/img/*',
+    'assets/img/**/*',
+    'assets/images/*',
+    'assets/images/**/*',
+  ])
+    .pipe(gulp.dest('static/images'));
 });
